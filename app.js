9e61fb565d061ea36e59bf8afa2db850083e8362
Sameerali0 <sameer.ali.professional@gmail.com>
2026-03-21 13:10:03 +0500

    add shop items and display them in UI

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const biryaniBtn = document.getElementById("biryani-btn");
 const biryaniCount = document.getElementById("biryani-count");
+const shopContainer = document.getElementById("shop-items");
 
 let totalBiryaniClick = 0;
 
@@ -11,3 +12,34 @@ function biryaniClick() {
 biryaniBtn.addEventListener("click", function () {
     biryaniClick();
 });
+
+const shopItems= [
+    {
+        name:"chef",
+        description: "chefs cook biryani for you",
+        cost: 10,
+        startingCost: 10,
+    },
+    {
+        name: "Masala",
+        description: "Double your biryani per click",
+        cost: 50,
+        startingCost: 50,
+    },
+]
+
+function createShopItems() {
+    shopItems.forEach(item =>{
+        const shopItem = document.createElement("div");
+        
+        shopItem.innerHTML =`
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <button>${item.cost}</button>
+        `;
+        
+        shopContainer.appendChild(shopItem);
+    });
+}
+
+createShopItems();
